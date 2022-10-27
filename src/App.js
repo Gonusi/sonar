d7120d61bd29ff918d4d0d81525e09c5feb4c4de
Gonusi <kasparasanusauskas@gmail.com>
2022-10-27 23:25:34 +0300

    working correlation graph

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import calculateCorrelation from "calculate-correlation";
 import "./App.css";
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let chirpSource;
 
+// TODO at this point, I'm able to record my own chirp from speakers
+// Try to correlate its channel with chirpBuffer content and display correlation on graph
+
 function App() {
   const [chirpBuffer, setChirpBuffer] = useState(null);
+  const correlationCanvas = useRef();
 
   // Load chirp file and put it into buffer
   useEffect(() => {
@@ -52,7 +57,69 @@ function App() {
         console.log("process callback");
         // Do something with the data, e.g. convert it to WAV
         console.log(e.inputBuffer);
-        const data = e.inputBuffer.getChannelData(0);
+        const recordedData = e.inputBuffer.getChannelData(0);
+        const sampleData = chirpBuffer.getChannelData(0);
+
+        console.log("recordedData.length", recordedData.length);
+        console.log("sampleData.length", sampleData.length);
+
+        // correlate
+        // take chirpBuffer.getChannelData(0)
+        // check each position in data array for correlation
+
+        const correlations = [];
+        recordedData.forEach((sample, index) => {
+          const slidingWindow = recordedData.slice(
+            index,
+            index + sampleData.length
+          );
+          if (slidingWindow.length < sampleData.length) return; // we're out of bounds
+          if (index === 0) {
+            console.log("correlating arrays:", slidingWindow, sampleData);
+          }
+          correlations[index] = calculateCorrelation(
+            Array.from(slidingWindow),
+            Array.from(sampleData)
+          );
+        });
+        console.log("correlations", correlations);
+
+        const correlationCanvasCtx = correlationCanvas.current.getContext("2d");
+        correlationCanvasCtx.fillStyle = "rgb(200, 200, 200)";
+        correlationCanvasCtx.fillRect(
+          0,
+          0,
+          correlationCanvas.current.width,
+          correlationCanvas.current.height
+        );
+
+        correlationCanvasCtx.lineWidth = 2;
+        correlationCanvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+        correlationCanvasCtx.beginPath();
+
+        const sliceWidth =
+          (correlationCanvas.current.width * 1.0) / correlations.length;
+        let x = 0;
+
+        for (let i = 0; i < correlations.length; i++) {
+          const v = correlations[i];
+          const y = v * correlationCanvas.current.height;
+
+          if (i === 0) {
+            correlationCanvasCtx.moveTo(x, y);
+          } else {
+            correlationCanvasCtx.lineTo(x, y);
+          }
+
+          x += sliceWidth;
+        }
+
+        correlationCanvasCtx.lineTo(
+          correlationCanvas.current.width,
+          correlationCanvas.current.height / 2
+        );
+        correlationCanvasCtx.stroke();
 
         const bufferSource = audioCtx.createBufferSource();
         bufferSource.buffer = e.inputBuffer;
@@ -79,6 +146,12 @@ function App() {
       >
         Play chirp
       </button>
+      <canvas
+        width={800}
+        height={500}
+        ref={correlationCanvas}
+        id="correlationCanvas"
+      ></canvas>
     </div>
   );
 }
