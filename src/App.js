c36329cfe4662f19c90e26b62ec2385659517b26
Gonusi <kasparasanusauskas@gmail.com>
2022-10-27 21:27:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let source;
 
 function App() {
+  const [chirpBuffer, setChirpBuffer] = useState(null);
+
+  useEffect(() => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "chirp_0050.ogg", true);
+    request.responseType = "arraybuffer";
+    request.onload = () => {
+      const audioData = request.response;
+      audioCtx.decodeAudioData(
+        audioData,
+        (buffer) => {
+          setChirpBuffer(buffer);
+        },
+
+        (err) => console.error(`Error with decoding audio data: ${err.err}`)
+      );
+    };
+
+    request.send();
+  }, []);
+
+  const loadChirp = () => {
+    source = audioCtx.createBufferSource();
+    source.buffer = chirpBuffer;
+    source.connect(audioCtx.destination);
+    source.loop = false;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          loadChirp();
+          source.start(0);
+          setTimeout(() => {
+            source.stop(0);
+          }, 150);
+        }}
+      >
+        Play chirp
+      </button>
     </div>
   );
 }
